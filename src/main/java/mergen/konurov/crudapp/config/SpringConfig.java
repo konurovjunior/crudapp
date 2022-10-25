56b78fb7b50bf7ce71c68ba21c9b4e1d90b05182
mergen.konurov <m.konurov@geeksoft.kz>
2022-10-25 23:44:08 +0600

    Deleting the database connection from .properties file

--- src/main/java/mergen/konurov/crudapp/config/SpringConfig.java
+++ src/main/java/mergen/konurov/crudapp/config/SpringConfig.java
@@ -26,12 +26,10 @@ import java.util.Objects;
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
-    private final Environment environment;
 
     @Autowired
     public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
-        this.environment = environment;
     }
 
     @Bean
@@ -62,10 +60,10 @@ public class SpringConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
-        dataSource.setUrl(environment.getProperty("url"));
-        dataSource.setUsername(environment.getProperty("username"));
-        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/crudapp");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("admin");
 
         return dataSource;
     }
